418a1bfca249c3aefc5db03b8f0aafe4071c63d3
Rutkovskaya Anastasiya <113971855+Nastuysha@users.noreply.github.com>
2023-04-05 23:55:02 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,11 +1,18 @@
 import pandas as pd
 import numpy as np
+import math
 
-
-chat_id = 123456 # Ваш chat ID, не меняйте название переменной
+chat_id = 1141722952
 
 def solution(x: np.array) -> float:
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
-    return x.mean() # Ваш ответ
+    mu = -39 + math.exp(1)
+    sample_size = len(x)
+    V0 = x[0]
+    ch = 0
+    zn = 0
+    t = 10
+    for i in range(sample_size):
+        ch += (x[i]-V0)
+        zn += (t - mu)
+    a = ch/zn
+    return a
