b46bc3e64403f67e57a7fbc1b45444dcc68af82b
Rutkovskaya Anastasiya <113971855+Nastuysha@users.noreply.github.com>
2023-04-06 21:16:59 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,18 +1,16 @@
 import pandas as pd
 import numpy as np
 import math
+from statistics import mean 
+
 
 chat_id = 1141722952
 
 def solution(x: np.array) -> float:
     mu = -39 + math.exp(1)
     sample_size = len(x)
-    V0 = x[0]
-    ch = 0
-    zn = 0
     t = 10
-    for i in range(sample_size):
-        ch += (x[i]-V0)
-        zn += (t - mu)
-    a = ch/zn
-    return a
+    a = []
+    for i in range(1, sample_size):
+        a.append((x[i] - x[i-1])/t)
+    return mean(a)
