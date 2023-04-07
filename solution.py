4d0881f22575096c3f38790cd6419356d9cbf609
Rutkovskaya Anastasiya <113971855+Nastuysha@users.noreply.github.com>
2023-04-07 21:08:43 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,12 +1,18 @@
 import pandas as pd
 import numpy as np
-
+import math
 
 chat_id = 1141722952
 
 def solution(x: np.array) -> float:
-    time = 10 
-    for i in range(len(x)):
-        x[i] /= (time)
-    a = np.mean(x)
+    mu = -39 + math.exp(1)
+    sample_size = len(x)
+    V0 = x[0]
+    ch = 0
+    zn = 0
+    t = 10
+    for i in range(sample_size):
+        ch += x[i]*(t-mu)
+        zn += (t - mu)**2
+    a = ch/zn
     return a
