988be3e96d6f86560d4476aebbdc4e22007bb3a2
Rutkovskaya Anastasiya <113971855+Nastuysha@users.noreply.github.com>
2023-04-07 23:50:35 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,18 +1,14 @@
 import pandas as pd
 import numpy as np
-import math
+
 
 chat_id = 1141722952
 
 def solution(x: np.array) -> float:
-    mu = -39 + math.exp(1)
-    sample_size = len(x)
-    V0 = x[0]
-    ch = 0
-    zn = 0
-    t = 10
-    for i in range(sample_size):
-        ch += x[i]*(t-mu)
-        zn += (t - mu)**2
-    a = ch/zn
+    # a = v/time, v - speed
+    time = 10 
+    n = len(x)
+    for i in range(len(x)):
+        sum_ai = x[i]/time
+    a = 1/n * sum_ai
     return a
